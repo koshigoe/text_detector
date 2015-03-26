2141a960ca58bd99ec5245364bfc9925ec4c5187
koshigoe <koshigoeb@gmail.com>
2015-03-26 21:16:27 +0900

    Add rspec examples.

--- spec/text_detector_spec.rb
+++ spec/text_detector_spec.rb
@@ -1,11 +1,31 @@
 require 'spec_helper'
 
 describe TextDetector do
-  it 'has a version number' do
-    expect(TextDetector::VERSION).not_to be nil
+  describe '.factory' do
+    subject { described_class.factory(:type, :dictionary) }
+
+    it do
+      expect(TextDetector::Executor).to receive(:new).with(:type, :dictionary)
+      subject
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.normalize' do
+    subject { described_class.normalize(text) }
+
+    context 'ひらがな' do
+      let(:text) { 'ひらがな' }
+      it { is_expected.to eq 'ヒラガナ' }
+    end
+
+    context '半角カナ' do
+      let(:text) { 'ﾊﾝｶｸｶﾅ' }
+      it { is_expected.to eq 'ハンカクカナ' }
+    end
+
+    context 'a\u0300' do
+      let(:text) { "a\u0300" }
+      it { is_expected.to eq "\u00E0" }
+    end
   end
 end
